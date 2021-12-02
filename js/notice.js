8b9994e4f5ec1dbd7df2d0f55579ef72680169b5
canplane <canplane@gmail.com>
2021-12-03 02:37:18 +0900

    211203-0237

--- js/notice.js
+++ js/notice.js
@@ -16,7 +16,7 @@ function modalDecline() {
 }
 
 
-function notice_clickEvent() {
+function friendRequestNotice_clickEvent() {
     $body.classList.add("modal-open");
     $modal.classList.add("receive_friend_request");
     $modal.innerHTML = `
@@ -34,4 +34,27 @@ function notice_clickEvent() {
     <li><button onclick="modalDecline()">거절</button></li>
 </ul>`;
 }
-$notices[1].addEventListener("click", notice_clickEvent);
+function groupInvitationNotice_clickEvent() {
+    $body.classList.add("modal-open");
+    $modal.classList.add("receive_group_invitation");
+    $modal.innerHTML = `
+<p class="modal-header">그룹 초대를 수락하시겠습니까?</p>
+<div class="modal-body">
+    <div class="group-item" style="background-image: linear-gradient(rgba(0, 0, 0, .6), rgba(0, 0, 0, .6)), url(resrc/group_image/16.jpg);">
+        <p class="name">놀러가자</p>
+        <div class="description">
+            <div class="num_of_members">
+                <span class="material-icons">account_circle</span>
+                <p>5명</p>
+            </div>
+        </div>
+    </div>
+</div>
+<ul class="modal-footer">
+    <li><button onclick="modalAccept()">승인</button></li>
+    <li><button onclick="modalDecline()">거절</button></li>
+</ul>`;
+}
+
+$notices[0].addEventListener("click", groupInvitationNotice_clickEvent);
+$notices[1].addEventListener("click", friendRequestNotice_clickEvent);
