43fea5244c541f835240667278b2fe88141fb454
canplane <canplane@gmail.com>
2021-12-02 18:06:57 +0900

    211202-1806

---
+++ js/notice.js
@@ -0,0 +1,36 @@
+const $body = document.querySelector("body");
+const $modal = document.querySelector("#modal");
+const $notices = document.querySelectorAll(".notice-item");
+
+
+function modalAccept() {
+    $body.classList.remove('modal-open');
+    $modal.innerHTML = "";
+}
+function modalDecline() {
+    $body.classList.remove('modal-open');
+    $modal.innerHTML = "";
+}
+
+
+function noticeClickEvent() {
+    $body.classList.add('modal-open');
+    $modal.innerHTML = `
+<p class="modal-header">친구 요청을 수락하시겠습니까?</p>
+<div class="modal-body">
+    <div class="user-item">
+        <div class="image">
+            <span class="material-icons">account_circle</span>
+        </div>
+        <p class="name">박재현</p>
+    </div>
+</div>
+<ul class="modal-footer">
+    <li><button onclick="modalAccept()">승인</button></li>
+    <li><button onclick="modalDecline()">거절</button></li>
+</ul>
+`
+}
+
+
+$notices[1].addEventListener("click", noticeClickEvent);
